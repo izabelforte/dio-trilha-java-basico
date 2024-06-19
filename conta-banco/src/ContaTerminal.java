e9df5dde4ade5055541b076e0a505bffc75ce1a4
Izabel Forte <izabelrforte@gmail.com>
2024-06-19 01:00:13 +0100

    add exemplos e desafio conta terminal

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //ToDo:Importar a classe Scanner
+
+        //ToDo:Exibir as mensagens para os usuários 
+
+        //ToDo:Obter os valores digitados
+    }
+}
