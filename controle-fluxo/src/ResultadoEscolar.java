e203146d3357c4c904694c287d83f0b5bb4030f1
Izabel Forte <izabelrforte@gmail.com>
2024-07-03 11:26:05 +0100

    add ex condicao ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,24 @@
 public class ResultadoEscolar {
    public static void main(String[] args) {
-      int nota = 10;
+      //condiciona encadeada
+      // int nota = 10;
 
-      if (nota >= 7)
-         System.out.println("Aprovado");
-      else if (nota >= 5 && nota < 7)
-         System.out.println("Prova de recuperação");
-      else
-         System.out.println("Reprovado");
+      // if (nota >= 7)
+      // System.out.println("Aprovado");
+      // else if (nota >= 5 && nota < 7)
+      // System.out.println("Prova de recuperação");
+      // else
+      // System.out.println("Reprovado");
+
+      //condicional ternária (composta)
+      // int nota = 7;
+      // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+      // System.out.println(resultado);
+
+      //condicional ternaria (encadeada)
+      int nota = 3;
+		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+		System.out.println(resultado);
+
    }
 }
