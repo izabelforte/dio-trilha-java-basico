7c334ad6868a040b6bb5490dd2a8b8dc9b356b45
Izabel Forte <izabelrforte@gmail.com>
2024-07-14 16:12:03 +0100

    Ex tratamento de excecoes

---
+++ controle-fluxo/src/FormatadorCepExemplo.java
@@ -0,0 +1,19 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            // TODO Auto-generated catch block
+            System.out.println("O CEP não é válido");
+        }
+    }
+   
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+        throw new CepInvalidoException();
+      
+        //simulando um cep formatado
+        return "23.765-064";
+    }
+}
